c14287b08667d52c745091bb430dce024be28617
fangyuwu@weflywifi.com <fangyuwu@weflywifi.com>
2025-03-09 22:14:53 +0800

    bugfix

--- OpenWrt/mt7981-rfb/diy-part2.sh
+++ OpenWrt/mt7981-rfb/diy-part2.sh
@@ -13,4 +13,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 cp ${GITHUB_WORKSPACE}/OpenWrt/mt7981-rfb/mt7981.dtsi target/linux/mediatek/files-6.6/arch/arm64/boot/dts/mediatek/ -f
-cp ${GITHUB_WORKSPACE}/OpenWrt/mt7981-rfb/Makefile package\boot\arm-trusted-firmware-mediatek/ -f
+cp ${GITHUB_WORKSPACE}/OpenWrt/mt7981-rfb/Makefile package/boot/arm-trusted-firmware-mediatek/ -f
